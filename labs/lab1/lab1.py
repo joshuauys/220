4a70f777ee099a2e4c795b28d8621c964d8746a8
joshuauys <97102576+joshuauys@users.noreply.github.com>
2022-01-18 21:40:42 -0500

    Add files via upload

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -14,13 +14,9 @@ net_balance = eval(input("What is your previous net balance?"))
 payment_amount = eval(input("What is your payment amount?"))
 payment_day = eval(input("What day in your billing cycle was a payment made?"))
 
-step1 = net_balance * days_in_billing_cycle
-step2 = payment_amount * (days_in_billing_cycle - payment_day)
-step3 = step1 - step2
-average_daily_balance = step3 / days_in_billing_cycle
+average_daily_balance = ((net_balance * days_in_billing_cycle) - (payment_amount * (days_in_billing_cycle - payment_day))) / days_in_billing_cycle
 
 monthly_interest_rate = annual_interest_rate / 100 / 12
 monthly_interest_charge = average_daily_balance * monthly_interest_rate
 
-print(average_daily_balance)
 print("Your monthly interest charge is $", round(monthly_interest_charge,2))
